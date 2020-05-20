9020f5a93a622ee2be22b90e564e8e837e61c727
FahrilPratama <m.fahrilpratama@gmail.com>
2020-05-21 01:35:44 +0700

    update project

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import ButtonBehavior
+from kivy.uix.label import Label
 from kivy.uix.image import Image
+from os import walk
+from functools import partial
 from workoutbanner import WorkoutsBanner
 import requests
 import json
@@ -12,11 +15,19 @@ class HomeScreen(Screen):
     pass
 
 
+class LabelButton(ButtonBehavior, Label):
+    pass
+
+
 class ImageButton(ButtonBehavior, Image):
     pass
 
 
 class SettingsScreen(Screen):
+    pass
+
+
+class ChangeAvatarScreen(Screen):
     pass
 
 
@@ -34,13 +45,24 @@ class MainApp(App):
         result = requests.get("https://fitnessapp-kivy.firebaseio.com/" + str(self.my_friend_id) + ".json")
         data = json.loads(result.content.decode())
 
+        # Populate avatar grid
+        avatar_grid = self.root.ids['change_avatar_screen'].ids['avatar_grid']
+        for root_dir, folders, files in walk("icons/avatars"):
+            for f in files:
+                img = ImageButton(source="icons/avatars/" + f, on_release=partial(self.change_avatar, f))
+                avatar_grid.add_widget(img)
+
         # Update avatar image
-        avatar_image = self.root.ids['home_screen'].ids['avatar_image']
+        avatar_image = self.root.ids['avatar_image']
         avatar_image.source = "icons/avatars/" + data['avatar']
 
         # Update streak label
         label_streak = self.root.ids['home_screen'].ids['streak_label']
         label_streak.text = str(data['streak']) + " Hari Berturut-turut"
+
+        # Update id label
+        friend_id_label = self.root.ids['settings_screen'].ids['friend_id_label']
+        friend_id_label.text = "ID: " + str(self.my_friend_id)
 
         # Get data workouts
         banner_grid = self.root.ids['home_screen'].ids['banner_grid']
@@ -50,6 +72,17 @@ class MainApp(App):
                                            type_image=workout['type_image'], number=workout['number'],
                                            units=workout['units'], likes=workout['likes'])
             banner_grid.add_widget(widget_banner)
+
+    def change_avatar(self, image, widget_id):
+        # Change avatar in app
+        avatar_image = self.root.ids['avatar_image']
+        avatar_image.source = "icons/avatars/" + image
+
+        # Change avatar in firebase
+        my_avatar = '{"avatar": "%s"}' % image
+        requests.patch("https://fitnessapp-kivy.firebaseio.com/" + str(self.my_friend_id) + ".json", data=my_avatar)
+
+        self.change_screen("settings_screen")
 
     def change_screen(self, screen_name):
         screen_manager = self.root.ids['screen_manager']
